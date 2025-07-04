541f37288fa09d8cb7e2537e11dca3c8f4421a2d
Igor <spitsyn.ia@phystech.edu>
2025-07-05 01:27:17 +0300

    Определение уровня пользователя

--- main.py
+++ main.py
@@ -2,17 +2,18 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_gigachat.chat_models import GigaChat
 from auth_key import get_api_key
 from PdfLoader import PDFProcessor
+from analysis_level import det_user_level
 
 key = get_api_key()
 giga = GigaChat(credentials=key, verify_ssl_certs=False,
 )
-
+level_promt = det_user_level(giga)
 pdf_path = "NLP1.pdf"
 documentation_promt = PDFProcessor(pdf_path=pdf_path).load_and_prepare_pdf()
-print(documentation_promt)
+
 messages = [
     SystemMessage(
-        content=documentation_promt
+        content=f"{level_promt}\n {documentation_promt}"
     )
 ]
 
