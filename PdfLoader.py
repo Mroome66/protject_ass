84187c4c83a14ead7e35a6c144fd3666d28843ba
Igor <spitsyn.ia@phystech.edu>
2025-07-05 00:48:09 +0300

    загрузка и обработка файла

---
+++ PdfLoader.py
@@ -0,0 +1,21 @@
+
+from langchain_community.document_loaders import PyPDFLoader
+
+class PDFProcessor:
+    def __init__(self, pdf_path: str):
+        self.pdf_path = pdf_path
+    
+    def load_and_prepare_pdf(self) -> str:
+        loader = PyPDFLoader(self.pdf_path)
+        pages = loader.load()
+        text = "\n".join([page.page_content for page in pages])
+        promt = """
+            Ты должен отвечать ТОЛЬКО на основе следующего документа:
+            {text}
+            Правила:
+            1. Если вопрос не относится к теме документа: "Это вне рамок предоставленных материалов"
+            2. Если требуется отвечать максимально понятно, то ты можешь пояснять термины развёрнуто.
+            3. Не придумывай информацию, которой нет в документе.
+            4. Для сложных вопросов объединяй информацию из разных частей документа
+            """
+        return promt
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_gigachat.chat_models import GigaChat
 from auth_key import get_api_key
+from PdfLoader import PDFProcessor
 
 key = get_api_key()
 giga = GigaChat(credentials=key, verify_ssl_certs=False,
 )
 
+pdf_path = "NLP1.pdf"
+documentation_promt = PDFProcessor(pdf_path=pdf_path).load_and_prepare_pdf()
+print(documentation_promt)
 messages = [
     SystemMessage(
         content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
