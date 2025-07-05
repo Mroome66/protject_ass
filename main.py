741d8b6ad3ecfc19e650bb14a101d36af323cfa5
Igor <spitsyn.ia@phystech.edu>
2025-07-05 17:45:23 +0300

    last

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_gigachat.chat_models import GigaChat
 from auth_key import get_api_key
@@ -5,27 +6,42 @@ from PdfLoader import PDFProcessor
 from analysis_level import det_user_level
 from Custom_GigaChat import LogGigaChat
 
+
 key = get_api_key()
-giga = LogGigaChat(credentials=key, log_file='log.txt',verify_ssl_certs=False,
-)
+giga =LogGigaChat(log_file='log.txt',credentials=key, verify_ssl_certs=False)
 
 
-level_promt = det_user_level(giga)
-pdf_path = "NLP1.pdf"
-documentation_promt = PDFProcessor(pdf_path=pdf_path).load_and_prepare_pdf()
+st.set_page_config(page_title="Диалог с GigaChat", layout="wide")
+st.title("Диалог с GigaChat")
 
-messages = [
-    SystemMessage(
-        content=f"{level_promt}\n {documentation_promt}"
-    )
-]
+if "messages" not in st.session_state:
+    level_prompt = det_user_level(giga)
+    pdf_path = "NLP.pdf"
+    documentation_prompt = PDFProcessor(pdf_path=pdf_path).load_and_prepare_pdf()
+    
+    st.session_state.messages = [
+        SystemMessage(content=f"{level_prompt}\n{documentation_prompt}")
+    ]
 
-while(True):
-    user_input = input("Пользователь: ")
-    if user_input == "пока":
-      break
-    messages.append(HumanMessage(content=user_input))
-    res = giga.invoke(messages)
-    print(res.content)
+for message in st.session_state.messages:
+    if isinstance(message, HumanMessage):
+        with st.chat_message("user"):
+            st.write(message.content)
+    elif isinstance(message, SystemMessage):
+        continue  
+    else:
+        with st.chat_message("assistant"):
+            st.write(message.content)
 
-  
+if prompt := st.chat_input("Введите ваше сообщение"):
+    st.session_state.messages.append(HumanMessage(content=prompt))
+   
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    with st.spinner("GigaChat думает..."):
+        response = giga.invoke(st.session_state.messages)
+        st.session_state.messages.append(response)
+    
+    with st.chat_message("assistant"):
+        st.write(response.content)
